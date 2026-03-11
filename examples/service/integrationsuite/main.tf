a1fa460b32895bfdabc2ccee33c3929e658fa965
TruLov <23336472+TruLov@users.noreply.github.com>
2026-03-11 09:14:37 +0100

    docs(service/integrationsuite): Add sample

---
+++ examples/service/integrationsuite/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    btp = {
+      source  = "SAP/btp"
+      version = "~> 1.20.0"
+    }
+  }
+}
+
+provider "btp" {
+  globalaccount = var.globalaccount
+}
+
+module "subaccount" {
+  source  = "gambit-consulting/foundation/btp//modules/subaccount"
+  version = "0.0.1"
+
+  name        = var.subaccount_name
+  subdomain   = var.subdomain
+  region      = var.region
+  description = "Subaccount for Integration Suite example"
+}
+
+module "integration_suite" {
+  source  = "gambit-consulting/foundation/btp//modules/service/integrationsuite"
+  version = "0.0.1"
+
+  subaccount_id = module.subaccount.id
+  service_name  = var.service_name
+  plan_name     = var.plan_name
+  quota_amount  = var.quota_amount
+}
